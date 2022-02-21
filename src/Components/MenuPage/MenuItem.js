88d581d7db31e54bc1f492f1bf6379af4ecfabb1
abdalrhman-samy <abdalrhman.samy@outlook.com>
2022-02-21 21:56:45 +0200

    adjustments for lighthouse

--- src/Components/MenuPage/MenuItem.js
+++ src/Components/MenuPage/MenuItem.js
@@ -8,7 +8,7 @@ function MenuItem({title, category}) {
   return (
     <motion.div key={title} initial={{opacity:0}} exit={{opacity: 0}} animate={{opacity: 1}} transition={{duration:0.25}}
     className='flex flex-col justify-between max-w-xs bg-black rounded-lg shadow-sm shadow-primary py-3 md:px-5 px-2 items-center text-center'>
-        <img src={category==="pizza"? pizzaImg : (category === "drinks" ? drinkImg : extrasImg)} alt="pizza" className='rounded-full mb-3'/>
+        <img src={category==="pizza"? pizzaImg : (category === "drinks" ? drinkImg : extrasImg)} alt="pizza" className='rounded-full mb-3' height="320" width="320"/>
         <div>
             <h6 className='text-xl font-bold'>{title}</h6>
             <span className='text-base font-semibold text-primary block'>20EGP</span>
